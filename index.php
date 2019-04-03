bcacd519945ef40f45213c47846880004be72ef5
Zane Stearman <zstearman@mail.greenriver.edu>
2019-04-03 17:35:37 -0400

    added a header

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//--only when in development
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Programming 1</title>
+
+</head>
+<body>
+<header>Pair Programming 1</header>
+
+</body>
+</html>
