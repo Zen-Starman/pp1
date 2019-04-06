0e3545b251cbb0707695eb02254cf434e1584602
Zane Stearman <zstearman@mail.greenriver.edu>
2019-04-05 20:21:15 -0400

    array change

--- index.php
+++ index.php
@@ -4,6 +4,18 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$numbers = array(7,9,8,9,8,8,6);
+
+function printArr($array){
+
+    foreach ($array as $item)
+        {
+            echo '<p>$item </p><br>';
+        }
+}
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,6 +26,7 @@ error_reporting(E_ALL);
 </head>
 <body>
 <header>Pair Programming 1</header>
+<div><?php printArr($numbers) ?></div>
 
 </body>
 </html>
